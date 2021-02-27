8c7af273b512c122158e24afc0ed1e2a63d13b2d
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2021-02-27 09:24:26 -0300

    day five

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,7 @@ import { CountdownProvider } from "../contexts/CountdownContext";
 import "../styles/global.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ChallengesProvider>
-      <Component {...pageProps} />
-    </ChallengesProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
